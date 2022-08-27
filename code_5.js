61af4b2b321f7460d0526506eeb6812e7c873aa9
Larissa <larissadecastro18@gmail.com>
2022-08-27 19:53:16 -0300

    Create a function that reverses an array

---
+++ code_5.js
@@ -0,0 +1,19 @@
+//5. Create a function that reverses an array
+
+function reverseArray(){
+    const numbers = [1,2,3,4,5,6,7,8,9,10]
+    console.log(numbers.reverse())
+}
+
+reverseArray()
+
+//
+
+function reverseArray(){
+    const string = ["tres", "dos", "uno"]
+    console.log(string.reverse())
+}
+
+reverseArray()
+
+
