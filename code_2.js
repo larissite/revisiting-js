1639b8d1c1fc1de7708cfd7ecae40e39a90e28b1
Larissa <larissadecastro18@gmail.com>
2022-08-27 19:47:51 -0300

    Print a table containing multiplication tables

---
+++ code_2.js
@@ -0,0 +1,10 @@
+function printMultiplicationTable(multiplier){
+        for(i = 1; i <= 10; i++){
+        console.log(`${multiplier} x ${i} = ${multiplier * i}`)
+
+    }
+}
+
+printMultiplicationTable(2)
+printMultiplicationTable(7)
+printMultiplicationTable(100)
